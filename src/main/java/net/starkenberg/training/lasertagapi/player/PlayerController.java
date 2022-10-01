2bfd4c719ec742d91a4aa89d6fc7f9ad9c79b252
Brad Starkenberg <brad@starkenberg.net>
2022-10-01 08:14:15 -0500

    refactor and optimize imports

--- src/main/java/net/starkenberg/training/lasertagapi/player/PlayerController.java
+++ src/main/java/net/starkenberg/training/lasertagapi/player/PlayerController.java
@@ -1,6 +1,5 @@
 package net.starkenberg.training.lasertagapi.player;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,33 +18,33 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/players")
 public class PlayerController {
-    private final PlayerRepository service;
+    private final PlayerRepository repo;
 
-    public PlayerController(PlayerRepository service) {
-        this.service = service;
-    }
-
-    @GetMapping(path = "/{id}")
-    public Player getById(@PathVariable String id) {
-        return service.getReferenceById(Long.valueOf(id));
-    }
-
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable String id) {
-        service.deleteById(Long.valueOf(id));
-        return new ResponseEntity<>(null, HttpStatus.OK);
+    public PlayerController(PlayerRepository repository) {
+        this.repo = repository;
     }
 
     @GetMapping
     public ResponseEntity<List<Player>> getPlayers() {
-        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Player getById(@PathVariable String id) {
+        return repo.getReferenceById(Long.valueOf(id));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        repo.deleteById(Long.valueOf(id));
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Player> newPlayer(@RequestBody PlayerForm playerForm, UriComponentsBuilder uriBuilder) {
         Player player = new Player();
         player.setCodeName(playerForm.getCodeName());
-        player = service.save(player);
+        player = repo.save(player);
         return ResponseEntity.created(
                 uriBuilder.path("/player/{id}").buildAndExpand(player.getId()).toUri()).build();
     }
