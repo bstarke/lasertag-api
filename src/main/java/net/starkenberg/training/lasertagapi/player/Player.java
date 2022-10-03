48f708dcbc09385e8d6198e6f51a62ec58bffb5b
Brad Starkenberg <brad@starkenberg.net>
2022-10-02 20:27:04 -0500

    remove generated from id

--- src/main/java/net/starkenberg/training/lasertagapi/player/Player.java
+++ src/main/java/net/starkenberg/training/lasertagapi/player/Player.java
@@ -4,14 +4,13 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class Player {
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id
+    @Column(unique = true)
     private Long id;
     @Column(name="codename", length = 30, unique = true)
     private String codeName;
