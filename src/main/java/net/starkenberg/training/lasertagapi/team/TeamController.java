cb83efe41485777e8bd51d58a7d2c0d3bc8c8c75
Brad Starkenberg <brstarkenberg.ctr@kr.af.mil>
2022-10-05 15:21:43 -0500

    WIP: Started working teams

--- src/main/java/net/starkenberg/training/lasertagapi/team/TeamController.java
+++ src/main/java/net/starkenberg/training/lasertagapi/team/TeamController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,15 @@ public class TeamController {
         team = repo.save(team);
         return ResponseEntity.created(
                 uriBuilder.path("/teams/{id}").buildAndExpand(team.getId()).toUri()).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Team> updateTeam(@PathVariable String id, @RequestBody Team team) {
+        if (team.getId() == Long.valueOf(id)) {
+            repo.save(team);
+            return new ResponseEntity<>(team, HttpStatus.OK);
+        } else {
+            throw new RuntimeException("IDs do not match");
+        }
     }
 }
