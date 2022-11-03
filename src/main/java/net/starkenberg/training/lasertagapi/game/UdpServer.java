00c49cdf5d188710454cea5b8a083ef04223af2e
Brad Starkenberg <brad@starkenberg.net>
2022-11-03 10:47:51 -0500

    add udp handler - next update model for frontend

---
+++ src/main/java/net/starkenberg/training/lasertagapi/game/UdpServer.java
@@ -0,0 +1,58 @@
+package net.starkenberg.training.lasertagapi.game;
+
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+
+public class UdpServer extends Thread {
+    private final DatagramSocket incomingSocket;
+    private final DatagramSocket outgoingSocket;
+    private boolean running;
+    private byte[] inbound = new byte[256];
+    private byte[] outbound = new byte[256];
+
+    public UdpServer() throws SocketException {
+        outgoingSocket = new DatagramSocket(7500);
+        incomingSocket = new DatagramSocket(7501);
+    }
+
+    @SneakyThrows
+    public void run() {
+        running = true;
+
+        while (running) {
+            DatagramPacket packet
+                    = new DatagramPacket(inbound, inbound.length);
+            incomingSocket.receive(packet);
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(inbound, inbound.length, address, port);
+            // incoming data int:int (player sending : player hit)
+            String received
+                    = new String(packet.getData(), 0, packet.getLength());
+            int sender = Integer.parseInt(received.substring(0, received.indexOf(":")));
+            int playerHit = Integer.parseInt(received.substring(received.indexOf(":")+1));
+            sendUdpReply(address, playerHit);
+            System.out.println(received);
+        }
+        incomingSocket.close();
+        outgoingSocket.close();
+    }
+
+    private void sendUdpReply(InetAddress address, int player) throws IOException {
+        inbound = ByteBuffer.allocate(Integer.SIZE/8).putInt(player).array();
+        DatagramPacket packet
+                = new DatagramPacket(outbound, outbound.length, address, 4445);
+        outgoingSocket.send(packet);
+    }
+
+    private void close() {
+        this.running = false;
+    }
+}
